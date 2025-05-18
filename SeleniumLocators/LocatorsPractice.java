2628be479ffccd7e3166371e9cdf9f0fa20aace8
Prassanna1995 <58099249+Prassanna1995@users.noreply.github.com>
2025-05-18 12:38:55 +0530

    Create LocatorsPractice.java

---
+++ SeleniumLocators/LocatorsPractice.java
@@ -0,0 +1,55 @@
+package Day22;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
+
+public class LocatorsDemo {
+
+	public static void staticWait() {
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		staticWait();
+		WebElement userName = driver.findElement(By.id("inputUsername"));
+		System.out.println(userName.isDisplayed());
+		userName.sendKeys("Prassanna");
+		staticWait();
+		WebElement pwd = driver.findElement(RelativeLocator.with(By.tagName("input")).below(userName));
+		System.out.println(pwd.isDisplayed());
+		pwd.sendKeys("Pwd");
+		staticWait();
+		List<WebElement> links=driver.findElements(By.tagName("a"));
+		System.out.println(links.size());
+		boolean resumeAssitance = driver
+				.findElement(By.linkText("Free Access to InterviewQues/ResumeAssistance/Material")).isDisplayed();
+		System.out.println(resumeAssitance);
+		boolean qaSummitLink = driver.findElement(By.partialLinkText("Rahul Shetty @Pune")).isDisplayed();
+		System.out.println(qaSummitLink);
+//		try {
+//			driver.findElement(By.tagName("img"));
+//		}catch (NoSuchElementException e){
+//			e.printStackTrace();
+//		}
+		List<WebElement> imgs=driver.findElements(By.tagName("id"));
+		System.out.println(imgs.size());
+		driver.quit();
+
+	}
+
+}
